9b415b2b8095b74e8bd03f6bedded12460cbb789
Christine <christinekhekama99@gmail.com>
2023-10-03 16:18:08 +0300

    react dom routes

--- kenha-frontend/src/App.jsx
+++ kenha-frontend/src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import Register from './Register';
 import Login from './Login';
 import Projects from './Projects';
@@ -7,15 +13,20 @@ import Contacts from './Contacts';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-      <Login />
-      <Projects />
-      <Services />
-      <Contacts />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
 
