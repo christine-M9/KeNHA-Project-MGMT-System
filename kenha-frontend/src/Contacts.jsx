4d5695399e6ce12a0bfdba595b25b09951d129eb
Christine <christinekhekama99@gmail.com>
2023-10-06 00:43:37 +0300

    fetching contacts data

--- kenha-frontend/src/Contacts.jsx
+++ kenha-frontend/src/Contacts.jsx
@@ -1,81 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
+  const [contacts, setContacts] = useState([]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch('/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data); 
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  };
+  useEffect(() => {
+    fetch('/contacts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setContacts(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   return (
-    <div style={{ padding: '20px', maxWidth: '400px', margin: 'auto' }}>
-      <h1 style={{ fontSize: '24px', marginBottom: '10px' }}>Contact Us</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={{ marginBottom: '10px' }}>
-          <input 
-            type="text" 
-            name="name" 
-            value={formData.name} 
-            onChange={handleChange} 
-            placeholder="Your Name" 
-            style={{ width: '100%', padding: '8px', fontSize: '16px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '10px' }}>
-          <input 
-            type="email" 
-            name="email" 
-            value={formData.email} 
-            onChange={handleChange} 
-            placeholder="Your Email" 
-            style={{ width: '100%', padding: '8px', fontSize: '16px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '10px' }}>
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Your Message"
-            style={{ width: '100%', padding: '8px', fontSize: '16px', minHeight: '100px' }}
-          />
-        </div>
-        <button type="submit" style={{ padding: '8px 16px', fontSize: '16px' }}>Submit</button>
-      </form>
+    <div style={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}>
+      <h1 style={{ fontSize: '24px', marginBottom: '20px' }}>Contact Data</h1>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {contacts.map(contact => (
+          <li key={contact.id} style={{ marginBottom: '20px', fontSize: '16px', lineHeight: '1.6' }}>
+            <strong>Name:</strong> {contact.name}, <strong>Email:</strong> {contact.email}, <strong>Message:</strong> {contact.message}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Contact;
+
