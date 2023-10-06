75270c6c4163e7db0d67df36f800286b24f6421f
Christine <christinekhekama99@gmail.com>
2023-10-06 16:54:14 +0300

    adding social media path

--- kenha-frontend/src/Footer.jsx
+++ kenha-frontend/src/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -14,6 +16,17 @@ const Footer = () => {
       <div style={{ maxWidth: '800px', margin: '0 auto' }}>
         <p style={{ marginBottom: '10px', fontSize: '1.4em' }}>
           By Christine Juma
+        </p>
+        <p>
+        <a href="https://github.com/christine-M9" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} style={{ margin: '0 10px' }} />
+          </a>
+          <a href="https://twitter.com/khekamejuma" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} style={{ margin: '0 10px' }} />
+          </a>
+          <a href="https://www.instagram.com/khekamejuma" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} style={{ margin: '0 10px' }} />
+            </a>         
         </p>
         <p>&copy; 2023 Kenya National Highways Authority</p>
       </div>
