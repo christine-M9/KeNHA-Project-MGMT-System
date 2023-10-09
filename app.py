754d7eaf11f9c8d15cd96a45b91e6e3dd928d4f8
Christine <christinekhekama99@gmail.com>
2023-10-09 12:25:35 +0300

    app path

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../kenha-frontend/dist', template_folder='../kenha-frontend/dist', static_url_path='')
 
 if __name__ == '__main__':
     app.run(debug=True)
